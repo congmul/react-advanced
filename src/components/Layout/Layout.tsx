06a3e05a56a9854b5fc4a41a9987cde8cb713f21
Jehyun <congmul@hotmail.com>
2022-12-29 09:34:53 -0800

    Add react-errorboundary package

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 function Layout():JSX.Element {
     return(
-        <div className="layout-basic"> {/* choose "layout-basic" or "layout-side" */}
+        <div className="layout-basic"> {/* choose "layout-basic" or "layout-side" */}            
             <Header />
             <main><Outlet /></main>
             <Footer>© 2022 - JEHYUN JUNG</Footer>
